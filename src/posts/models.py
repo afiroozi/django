1355a3c86cfdc44c9e4b19aa84c6be5dfb10cf26
afiroozi <amir.firoozi@gmail.com>
2018-02-07 11:26:35 -0500

    fix bug

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
@@ -9,8 +7,7 @@ class Post(models.Model):
 	updated = models.DateTimeField(auto_now=True, auto_now_add = False)
 	timestamp = models.DateTimeField(auto_now = False,auto_now_add=True)
 
-	def __unicode__(self):
-		return self.title	
+	
 
-	def __init__(self):
+	def __str__(self):
 		return self.title
